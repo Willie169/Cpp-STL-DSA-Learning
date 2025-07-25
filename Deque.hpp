98f40e77e21bdeb3b16bfdd7adcd76238d76f139
Wiilie169 <cg10250207@gmail.com>
2025-07-25 11:26:43 +0800

    m

--- Deque.hpp
+++ Deque.hpp
@@ -6,11 +6,10 @@ template<class T>
 class Deque {
     T** map;
     std::size_t sz, sb, si, eb, ei;
-    constexpr size_t __deque_buf_size(size_t sz) {
-        return sz < 512 ? size_t(512 / sz) : size_t(1);
-    }
+    static const size_t __deque_buf_size =  sizeof(T) < 512 ? size_t(512 / sizeof(T)) : size_t(1);
 
 public:
     constexpr deque() : map(nullptr), sz(0), sb(0), si(0), eb(0), ei(0);
 
-    explicit constexpr deque(std::size_t count) : 
+    explicit constexpr deque(std::size_t count) {
+        std::size_t blocks_needed = (count + block_size - 1) / block_size; 
