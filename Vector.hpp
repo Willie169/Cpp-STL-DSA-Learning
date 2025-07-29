cf4ee25cb53e946aeda8eaa4d38e778fdeab4b48
Willie Shen <cg10250207@gmail.com>
2025-07-29 16:52:01 +0800

    m

--- Vector.hpp
+++ Vector.hpp
@@ -32,9 +32,9 @@ public:
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
     using reference = T&;
-    using const reference = const T&;
+    using const_reference = const T&;
     using pointer = T*;
-    using const pointer = const T*;
+    using const_pointer = const T*;
     using iterator = T*;
     using const_iterator = const T*;
     using reverse_iterator = std::reverse_iterator<Iterator>;
