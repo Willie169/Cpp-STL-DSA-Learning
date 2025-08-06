798b012d546813321004602b7314f4be431fd804
Wiilie169 <cg10250207@gmail.com>
2025-08-06 18:46:16 +0800

    m

--- Array.hpp
+++ Array.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <algorithm>
+#include <compare>
 #include <cstddef>
 #include <iterator>
 #include <stdexcept>
+#include <type_traits>
 #include <utility>
 
 template<class T, std::size_t N>
@@ -20,17 +22,25 @@ struct Array {
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+
+private:
+
     T elems[N == 0 ? 1 : N];
+
+
+public:
 
     constexpr T& at(std::size_t index) {
         if (index >= N) throw std::out_of_range("Array");
         return elems[index];
     }
 
+
     constexpr const T& at(std::size_t index) const {
         if (index >= N) throw std::out_of_range("Array");
         return elems[index];
     }
+
 
     constexpr T& operator[](std::size_t index) { return elems[index]; }
     constexpr const T& operator[](std::size_t index) const { return elems[index]; }
@@ -43,6 +53,7 @@ struct Array {
 
     constexpr T* data() noexcept { return elems; }
     constexpr const T* data() const noexcept { return elems; }
+
 
     constexpr T* begin() noexcept { return elems; }
     constexpr const T* begin() const noexcept { return elems; }
@@ -60,12 +71,75 @@ struct Array {
     constexpr std::reverse_iterator<const T*> rend() const noexcept { return std::reverse_iterator<const T*>(begin()); }
     constexpr std::reverse_iterator<const T*> crend() const noexcept { return std::reverse_iterator<const T*>(begin()); }
 
+
     constexpr bool empty() const noexcept { return N == 0; }
     constexpr std::size_t size() const noexcept { return N; }
     constexpr std::size_t max_size() const noexcept { return N; }
-
     constexpr void fill(const T& value) { for (T& elem : *this) elem = value; }
+
 
     constexpr void swap(Array& other) noexcept(std::is_nothrow_swappable_v<T>) { for (std::size_t i = 0; i < N; ++i) std::swap(elems[i], other.elems[i]); }
 };
 
+
+template<class T, std::size_t N>
+constexpr bool operator==(const Array<T, N>& lhs, const Array<T, N>& rhs) {
+    return std::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+// Non-member operator<=> (C++20 three-way comparison)
+template<class T, std::size_t N>
+constexpr std::compare_three_way_result_t<T> operator<=>(const Array<T, N>& lhs, const Array<T, N>& rhs) {
+    return std::lexicographical_compare_three_way(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+
+
+template<class T, std::size_t N>
+constexpr void swap(Array<T, N>& lhs, Array<T, N>& rhs) noexcept(noexcept(lhs.swap(rhs))) { lhs.swap(rhs); }
+
+
+template<class T, std::size_t N>
+constexpr Array<std::remove_cv_t<T>, N> to_array(T (&arr)[N]) { return [&arr]<std::size_t... I>(std::index_sequence<I...>) { return Array<std::remove_cv_t<T>, N>{arr[I]...}; }(std::make_index_sequence<N>{}); }
+
+
+template<class T, std::size_t N>
+constexpr Array<std::remove_cv_t<T>, N> to_array(T (&&arr)[N]) { return [&arr]<std::size_t... I>(std::index_sequence<I...>) { return Array<std::remove_cv_t<T>, N>{std::move(arr[I])...}; }(std::make_index_sequence<N>{}); }
+
+
+template<class T, std::size_t N>
+struct std::tuple_size<Array<T, N>> : std::integral_constant<std::size_t, N> {};
+
+
+template<std::size_t I, class T, std::size_t N>
+struct std::tuple_element<I, Array<T, N>> {
+    static_assert(I < N, "Index out of bounds for Array");
+    using type = T;
+};
+
+
+template<std::size_t I, class T, std::size_t N>
+constexpr T& get(Array<T, N>& arr) noexcept {
+    static_assert(I < N, "Index out of bounds for Array");
+    return arr[I];
+}
+
+
+template<std::size_t I, class T, std::size_t N>
+constexpr T&& get(Array<T, N>&& arr) noexcept {
+    static_assert(I < N, "Index out of bounds for Array");
+    return std::move(arr[I]);
+}
+
+
+template<std::size_t I, class T, std::size_t N>
+constexpr const T& get(const Array<T, N>& arr) noexcept {
+    static_assert(I < N, "Index out of bounds for Array");
+    return arr[I];
+}
+
+
+template<std::size_t I, class T, std::size_t N>
+constexpr const T&& get(const Array<T, N>&& arr) noexcept {
+    static_assert(I < N, "Index out of bounds for Array");
+    return std::move(arr[I]);
+}
