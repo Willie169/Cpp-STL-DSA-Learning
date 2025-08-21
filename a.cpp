e120432661e2d4ef779203bda607f2c3235751c8
Wiilie169 <cg10250207@gmail.com>
2025-08-21 18:27:34 +0800

    m

--- a.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <typeinfo>
-#include <cxxabi.h>
-int main() {
-    int status;
-    std::cout << *(abi::__cxa_demangle(typeid(std::vector<bool, std::allocator<int>>::allocator_type).name(), 0, 0, &status)) << "\n";
-    return 0;
-}
