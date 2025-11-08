e7d0d304cbd92410a8db92208f0650f8cf18c19f
Willie Shen <cg10250207@gmail.com>
2025-11-09 01:25:01 +0800

    c

--- bits/range-access.hpp
+++ bits/range-access.hpp
@@ -73,4 +73,25 @@ constexpr std::size_t size( const T (&array)[N] ) noexcept { return N; }
 template< class T, std::ptrdiff_t N >
 constexpr std::ptrdiff_t ssize( const T (&array)[N] ) noexcept { return N; }
 
+template< class C >
+constexpr auto empty( const C& c ) -> decltype(c.empty()) { return c.empty(); }
+
+template< class T, std::size_t N >
+constexpr bool empty( const T (&array)[N] ) noexcept { return false; }
+
+template< class E >
+constexpr bool empty( std::initializer_list<E> il ) noexcept { return il.size() == 0; }
+
+template< class C >
+constexpr auto data( C& c ) -> decltype(c.data()) { return c.data(); }
+
+template< class C >
+constexpr auto data( const C& c ) -> decltype(c.data()) { return c.data(); }
+
+template< class T, std::size_t N >
+constexpr T* data( T (&array)[N] ) noexcept { return array; }
+
+template< class E >
+constexpr const E* data( std::initializer_list<E> il ) noexcept { return il.begin(); }
+
 } // namespace mystd
